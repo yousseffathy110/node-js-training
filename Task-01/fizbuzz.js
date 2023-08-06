d8777bfda9268b481124f5dacb2ae97bd2ed9ab0
yousef fathy <yf1102003@gmail.com>
2023-08-06 17:58:39 +0300

    Finsh

--- Task-01/fizbuzz.js
+++ Task-01/fizbuzz.js
@@ -1,13 +1,11 @@
-for(let i = 1; i <= 15 ; i++){
-    if(i % 3 == 0){
+for (let i = 0; i <= 15; i++) {
+    if (i % 3 == 0) {
         console.log("fizz")
-    }
-    else if(i % 5 == 0){
+    }else if (i % 5 == 0) {
         console.log("buzz")
-    }
-    else if(i % 15 == 0){
+    }else if (i % 15 == 0) {
         console.log("fizzbuzz")
+    }else{
+        console.log(i)
     }
-    else 
-        console.log(i);
 }
