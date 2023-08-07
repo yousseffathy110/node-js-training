2064bfde3f9852290659061897f85c338f46cab8
yousef fathy <yf1102003@gmail.com>
2023-08-07 23:53:45 +0300

    Solved

---
+++ Task-01/7.js
@@ -0,0 +1,17 @@
+let input = prompt("Enter A Number: ");
+let number = parseInt(input);
+
+function Multiplies(x) {
+  if (x === 0 || x < 0) {
+    return "Not A Valid Value!";
+  } 
+  else {
+    if (x % 5 === 0 || x % 8 === 0) {
+      return "Multiplies!!!";
+    } else {
+      return "Not Multiplies!";
+    }
+  }
+}
+
+console.log(Multiplies(number));
