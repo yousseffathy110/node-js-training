c687ef130fe8fe356096b12786542fcb50d55552
yousef fathy <yf1102003@gmail.com>
2023-08-07 23:40:58 +0300

    Solved

---
+++ Task-01/6.js
@@ -0,0 +1,7 @@
+let input = prompt("Enter A Number : ")
+if(input == 0)
+    console.log("Not Positive OR Negative")
+else if(input > 0)
+    console.log("The number is  Positive")
+else
+    console.log("The Number Is Negative ")
