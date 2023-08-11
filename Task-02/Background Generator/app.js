d815ebb8e0f3c522fcef6f9ada93adc10caad73d
yousef fathy <yf1102003@gmail.com>
2023-08-11 03:22:33 +0300

    Process

--- Task-02/Background Generator/app.js
+++ Task-02/Background Generator/app.js
@@ -1,8 +1,35 @@
-const color1 = document.querySelector(".input-color1")
-const color2 = document.querySelector(".input-color2")
-const body = document.querySelector(".body")
+const input1 = document.querySelector(".color1")
+const input2 = document.querySelector(".color2")
+const body = document.querySelector("body")
+const light = document.querySelector(".light")
+const dark = document.querySelector(".dark")
+const h1 = document.querySelector("h1")
+const random = document.querySelector(".random-btn")
 
-function change_Backgroung(){
-    body.style.background = "linear-gradient(to right, " + color1.value + ", " 
-	+ color2.value +")";
+
+function SetGradiaentBackgroungToBody(){
+    body.style.background = `linear-gradient(to right, ${input1.value}, 
+    ${input2.value})`
 }
+
+function DarkMode(){
+    body.style.background = "#2d2d2d";
+    h1.style.color = "#FFFFFF"
+}
+
+
+function LightMode(){
+    body.style.background = "#FFFFFF";
+    h1.style.color = "#2d2d2d"
+    dark.style.background = "#2d2d2d"
+    dark.style.color = "#ffffff"
+    light.style.background = "#2d2d2d"
+    light.style.color = "#ffffff"
+    random.style.background = "#2d2d2d"
+    random.style.color = "#ffffff"
+}
+
+input1.addEventListener("input", SetGradiaentBackgroungToBody)
+input2.addEventListener("input", SetGradiaentBackgroungToBody)
+dark.addEventListener("click", DarkMode)
+light.addEventListener("click", LightMode)
