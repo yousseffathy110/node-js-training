3e01d1d7ad424e5f5fd34d95c5a1081524df38fe
yousef fathy <yf1102003@gmail.com>
2023-08-17 09:26:17 +0300

    Done !

---
+++ Task-03/work.js
@@ -0,0 +1,81 @@
+// let num = 10;
+// function outer(){
+
+//     function inner(){
+//         let num = 20;
+//     }
+
+//     console.log(num)
+// }
+// outer();  
+//the output 10
+
+
+// *******************************************************************
+
+
+// let person = {
+//     name : "john",
+//     getname : function(){
+//         return name;
+//     }
+// }
+
+// console.log(person.getname())
+// error because the this keyword
+
+// *************************************************************************
+
+
+// const i;
+// for(i = 1; i < 5; i++){
+
+// }
+// console.log(i)
+// error because the const var cannot be assigned
+
+
+// ************************************************************************
+
+
+// const arr = [1, 2, 3, 4, 5]
+//     ,newarr = [];
+// for(let val in arr){
+//     newarr.push(val);
+//     console.log(newarr)
+// }
+// 1  
+// 1 2 
+// 1 2 3 
+// 1 2 3 4 
+
+
+// ******************************************************************
+
+
+// const arr = [1, 2, 3, 4, 5],
+// [foo, bar, ...baz] = arr
+
+// console.log(foo)
+// console.log(bar)
+// console.log(baz)
+// 1
+// 2
+// [ 3, 4, 5 ]
+
+
+// ***************************************************************
+
+
+function addvalues(...values){
+    // Write Js Code To Add 1 +2 + 3 + 4
+
+    let sum = 0;
+
+    for(let i = 0 ; i < values.length ; i++){
+        sum += values[i];
+    }
+
+    console.log(sum)
+}
+addvalues([1, 2, 3, 4, 5])
