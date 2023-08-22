e6e108851e82a498dd72990526fb2458249f6358
yousef fathy <yf1102003@gmail.com>
2023-08-22 14:47:00 +0300

    Done

--- Task-05/src/GeometricObject Interface and its Implementation Classes Circle and Rectangle.ts
+++ Task-05/src/GeometricObject Interface and its Implementation Classes Circle and Rectangle.ts
@@ -16,7 +16,22 @@ class circle implements GeometricObject{
         return 2 * Math.PI * this.radius
     }
 }
+class rectangle implements GeometricObject{
+    private width : number
+    private length : number
+    
+    constructor(width : number , length : number){
+        this.width = width
+        this.length  =length
+    }
 
+    getArea(): number {
+        return this.width * this.length
+    }
+    getPerimeter(): number {
+        return (this.width + this.length) * 2
+    }
+}
 const C = new circle(5.7)
 console.log(C.getArea())
 console.log(C.getPerimeter())
