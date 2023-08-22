1612f411eb255f8214b4ae35bb5635731a5706e7
yousef fathy <yf1102003@gmail.com>
2023-08-22 19:56:02 +0300

    Done

--- Quiz-02/dist/sol4.js
+++ Quiz-02/dist/sol4.js
@@ -1,1 +1,13 @@
 "use strict";
+function isPrime(x) {
+    if (x < 2) {
+        return false;
+    }
+    for (let i = 2; i <= Math.sqrt(x); i++) {
+        if (x % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+console.log(isPrime(5));
---
+++ Quiz-02/src/sol4.ts
@@ -0,0 +1,17 @@
+function isPrime(x: number){
+    if (x < 2) {
+      return false;
+    }
+  
+    for (let i = 2; i <= Math.sqrt(x); i++) {
+      if (x % i === 0) {
+        return false;
+      }
+    }
+  
+    return true;
+}
+
+
+console.log(isPrime(5)) // true
+  
